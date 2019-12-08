ff6c41a290dbb8530917530ffea597df867e5461
akira noguchi <guppy0356.nakira@gmail.com>
2019-12-08 09:38:56 +0000

    Add new feature: print year

--- spec/happy_spec.rb
+++ spec/happy_spec.rb
@@ -1,5 +1,15 @@
+require 'timecop'
+
 RSpec.describe Happy do
+  before do
+    Timecop.freeze(Time.local(2017))
+  end
+
+  after do
+    Timecop.return
+  end
+
   it "prints 'Happy new year!" do
-    expect(Happy::New.year).to eq('Happy new year!')
+    expect(Happy::New.year).to eq('Happy new year! 2017!')
   end
 end
